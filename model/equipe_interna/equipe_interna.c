18f3033c30cd92afa71f4395826e09ada010b7a7
sofiaarantes <sofiaarant3s@gmail.com>
2025-09-23 12:06:04 -0300

    Salvar incompleto

--- model/equipe_interna/equipe_interna.c
+++ model/equipe_interna/equipe_interna.c
@@ -2,9 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include "equipe_interna.h"
+#include "model/config_armazenamento/config_armazenamento.h"
 
 // Cria e inicializa um funcionário da equipe interna
-EquipeInterna* adicionar_funcionario_eqIn(int id, const char* nome, int cpf, const char* funcao, float valor_diaria) {
+EquipeInterna* adicionar_funcionario(int id, const char* nome, int cpf, const char* funcao, float valor_diaria) {
+    TipoArmazenamento tipo = get_armazenamento();
+    switch(tipo) {
+        case MEMORIA:
+            memoria[qtd++] = f;
+            printf("Funcionario salvo em MEMORIA.\n");
+            break;
+        
+        case TEXTO:
+            FILE* fp = fopen("equipe.txt", "a");
+            if (!fp) return;
+            fprintf(fp, "%d;%s;%d;%s;%.2f\n", f->id, f->nome, f->cpf, f->funcao, f->valor_diaria);
+            fclose(fp);
+            printf("Funcionario salvo em TEXTO.\n");
+            break;
+        
+        case BINARIO:
+            FILE* fp = fopen("equipe.bin", "ab"); // append binário
+            if (!fp) return;
+            fwrite(f, sizeof(EquipeInterna), 1, fp);
+            fclose(fp);
+            printf("Funcionario salvo em BINARIO.\n");
+            break;
+    }
     EquipeInterna* novo = (EquipeInterna*) malloc(sizeof(EquipeInterna));
     if (!novo) {
         return NULL; // erro de memória
